e7f1cf7089565a46b8a26489d2825bc4a0ce21f5
Larry <larryjamestaylor@gmail.com>
2017-08-18 09:20:09 -0700

    build constructor and display list of animals

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,21 @@ import { Component } from '@angular/core';
   template: `
   <div class='container'>
   <h1>Zuckerman's Zany Zoo</h1>
-  <h1>Zoo inhabitants on {{month}}/{{day}}/{{year}}</h1>
+  <h3>Zoo inhabitants on {{month}}/{{day}}/{{year}}</h3>
 
   <ul>
-    <li>{{firstAnimal.description}}</li>
+    <li *ngFor="let currentAnimal of animals">{{currentAnimal.species}}
+      <ul>
+        <li>{{currentAnimal.name}}</li>
+        <li>{{currentAnimal.age}} years old</li>
+        <li>{{currentAnimal.diet}}</li>
+        <li>{{currentAnimal.location}}</li>
+        <li>{{currentAnimal.caretakers}} caretakers required</li>
+        <li>{{currentAnimal.sex}}</li>
+        <li>likes {{currentAnimal.likes}}</li>
+        <li>doesn't like {{currentAnimal.dislikes}}</li>
+      </ul>
+    </li>
   </ul>
   </div>
   `
@@ -22,7 +33,13 @@ export class AppComponent {
     day: number = this.currentTime.getDate();
     year: number = this.currentTime.getFullYear()
 
-    firstAnimal = {
-      description: "tiger"
+    animals: Animal[] = [
+      new Animal('Bengal Tiger', 'Shere Khan', 6, 'carnivore', 'southeast asian jungle', 3, 'male', 'intimidation', 'mancubs'),
+      new Animal('Emperor Penguin', 'James Bond', 2, 'carnivore', 'antarctic', 1, 'female', 'sliding on ice', 'orcas')
+    ]
     }
+
+export class Animal {
+  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) { }
+}
 }
